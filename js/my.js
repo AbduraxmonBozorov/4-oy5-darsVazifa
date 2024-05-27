2ca6ae1497a249286bf6b5362c5330172208871c
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-05-27 07:12:57 +0500

    chalasi bor iltimos dars vaqtidan keyin tekshiring

---
+++ js/my.js
@@ -0,0 +1,13 @@
+// 1.1-masala
+// let n=+prompt("n soniga qiymat kiriting");
+// for(let i=1; i<=n; i++){
+//     console.log(i);
+// }
+
+
+
+// 1.2-masala
+// let n=+prompt("n soniga qiymat kiriting");
+// for(let i=n; i>=1; i--){
+//     console.log(i);
+// }
