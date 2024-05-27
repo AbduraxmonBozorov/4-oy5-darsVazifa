83d697cb0375c793c797102fe69a69b193dd1791
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-05-27 13:52:24 +0500

    2 topshiriq 9-masalasi bo'ldi

--- js/my.js
+++ js/my.js
@@ -4,10 +4,160 @@
 //     console.log(i);
 // }
 
-
-
 // 1.2-masala
 // let n=+prompt("n soniga qiymat kiriting");
 // for(let i=n; i>=1; i--){
 //     console.log(i);
 // }
+
+// 1.3-masala
+// let n=+prompt("n soniga qiymat kiriting");
+// let summ=0;
+// for(let i=0; i<=n; i++){
+//     summ+=i
+// }
+// console.log(summ);
+//
+
+// 1.4-masala
+// let n=+prompt("n soniga qiymat kiriting");
+// let summ=0;
+// for(let i=0; i<=n; i++){
+//     if(n%2==0){
+//         summ+=i;
+//         console.log(i);
+//     }
+// }
+// console.log(summ);
+
+// 1.5-masala
+// let n=+prompt("n soniga qiymat kiriting");
+// let summ=0;
+// for(let i=0; i<=n; i++){
+//     if(n%2==1){
+//         summ+=i;
+//         console.log(i);
+//     }
+// }
+// console.log(summ);
+
+// 1.6-masala
+// let n=+prompt("n soniga qiymat kiriting");
+// for(let i=1; i<=n; i++){
+//     if(n%i==0){
+//         console.log(i);
+//     }
+// }
+
+// 1.7-masala
+// let n=+prompt("n soniga qiymat kiriting");
+// let count=0;
+// let sum=0
+// for(let i=1; i<=n; i++){
+//     if(n%i==0){
+//         console.log(i);
+//         count++;
+//         sum+=i;
+//     }
+// }
+
+// 1.7-masala
+// let count=0
+// let n=+prompt("n soniga qiymat kiriting");
+// for(let i=1; i<=n; i++){
+//     if(n%i==0){
+//         count++;
+//     }
+// }
+// if(count==2){
+//     console.log("Kiritilgan son tub");
+// }
+// else{
+//     console.log("Kiritilgan son tub emas");
+// }
+
+// // 2.1-masala
+// let n=+prompt("n sonini kiriting");
+// let k=+prompt("k sonini kiriting");
+
+// for(let i=1; i<=n; i++){
+//     console.log(i+" - "+k);
+// }
+
+// // 2.2-masala
+// let a=+prompt("a sonini kiriting");
+// let b=+prompt("b sonini kiriting");
+// for(let i=a; i<=b; i++){
+//     console.log(parseInt(i));
+// }
+
+// 2.3-masala
+// let a=+prompt("a sonini kiriting");
+// let b=+prompt("b sonini kiriting");
+// for(let i=b; i>=a; i--){
+//     console.log(parseInt(i));
+// }
+
+// 2.4-masala
+// let price=50000;
+// for (let i = 1; i <= 10; i++) {
+  // let totalPrice=i*price;
+  // console.log(totalPrice);
+// }
+
+// 2.5-masala
+// let price=50000;
+// for(let i=0.1; i<=1; i+=0.1){
+//     console.log(price*i);
+// }
+
+// 2.6-masala
+// let price=50000;
+// for(let i=1.2; i<=2; i+=0.2){
+//     console.log(price*i);
+// }
+
+
+
+
+
+// 2.7-masala
+// let a=+prompt("A sonini kiriting");
+// let b=+prompt("b sonini kiriting");
+// a=parseInt(a);
+// b=parseInt(b);
+// let summ=0;
+// for(let i=a; i<=b; i++){
+//     summ+=i;
+// }
+// console.log(summ);
+
+
+
+// 2.8-masala
+// let a=+prompt("A sonini kiriting");
+// let b=+prompt("b sonini kiriting");
+// a=parseInt(a);
+// b=parseInt(b);
+// let summ=1;
+// for(let i=a; i<=b; i++){
+//     summ*=i;
+// }
+// console.log(summ);
+
+
+
+// 2.9-masala
+// let a=+prompt("A sonini kiriting");
+// let b=+prompt("b sonini kiriting");
+// a=parseInt(a);
+// b=parseInt(b);
+// let summ=1;
+// for(let i=a; i<=b; i++){
+//     summ*=i**2;
+// }
+// console.log(summ);
+
+
+// 2.10-masala
+let n=+prompt("")
